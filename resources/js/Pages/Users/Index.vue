7efdc4b4265bcdb0f66a2078dd76ab0e91b5793c
Dominik <dominik.stypula1@gmail.com>
2023-11-11 20:24:40 +0100

    Update Index.vue

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -11,6 +11,7 @@
     <input v-model="search" type="text" placeholder="Search..." class="border px-2 rounded-lg" />
   </div>
 
+  <!-- see https://tailwindui.com/ for examples of tables, forms etc. -->
   <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
